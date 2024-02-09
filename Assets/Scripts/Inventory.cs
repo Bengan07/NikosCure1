8bd90334d89c5e6d46c738f35d844edab230e6d9
Alva Bengtsson <alva.bengtsson1@learnet.se>
2024-02-09 09:15:19 +0100

    Inventorytest1

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InventoryItemData : MonoBehaviour
+public class Inventory : MonoBehaviour
 {
+    public bool[] isFull;
+    public GameObject[] slots;
+
     // Start is called before the first frame update
     void Start()
     {
