78cb9278b24fc2c567f2676eecce46c588506461
Alva Bengtsson <alva.bengtsson1@learnet.se>
2024-03-12 11:00:41 +0100

    feuefje

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -6,9 +6,14 @@ using UnityEngine.SceneManagement;
 public class SceneLoader : MonoBehaviour
 {
     // Start is called before the first frame update
-    public void PlayButton()
+    public void TryAgainButton()
     {
         SceneManager.LoadScene("Starter World");
+    }
+
+    public void MainMenuButton()
+    {
+        SceneManager.LoadScene("Main Menu");
     }
 
     public void QuitButton()
@@ -16,12 +21,22 @@ public class SceneLoader : MonoBehaviour
         Application.Quit();
     }
 
+
+    public GameObject Canvas;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-           SceneManager.LoadScene("Pause Menu");
+            // Show the "Pause Menu" canvas
+            Canvas.SetActive(true);
+        }
+        else
+        {
+            // Hide the "Pause Menu" canvas
+            Canvas.SetActive(false);
         }
     }
 
+    // Add the following line to declare the Canvas object
+    
 }
