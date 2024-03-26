a4ab6bd93b62c63c9120fd5ea3ca44aff7dea1fe
Alva Bengtsson <alva.bengtsson1@learnet.se>
2024-03-26 09:08:18 +0100

    en main menu + lite annat jag jobbat på

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -3,40 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneLoader : MonoBehaviour
+public static class SceneLoader 
 {
-    // Start is called before the first frame update
-    public void TryAgainButton()
+    public enum Scene
+    {
+
+    }
+    public static void Load()
     {
         SceneManager.LoadScene("Starter World");
     }
 
-    public void MainMenuButton()
-    {
-        SceneManager.LoadScene("Main Menu");
-    }
-
-    public void QuitButton()
-    {
-        Application.Quit();
-    }
 
 
-    public GameObject PauseCanvas;
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            // Show the "Pause Menu" canvas
-            PauseCanvas.SetActive(true);
-        }
-        else
-        {
-            // Hide the "Pause Menu" canvas
-            PauseCanvas.SetActive(false);
-        }
-    }
-
-    // Add the following line to declare the Canvas object
-    
 }
